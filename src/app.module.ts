1ae4c3f6373e0865a4ca367e750e3fa7ad307261
Nurdaulet Satbergen <nurik93sat@gmail.com>
2026-07-14 17:27:26 +0500

    Added users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './libs/database/prisma.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule]
+  imports: [ConfigModule.forRoot(), PrismaModule, UsersModule]
 })
 export class AppModule {}
