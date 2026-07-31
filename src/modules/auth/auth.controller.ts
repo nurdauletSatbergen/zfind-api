3ef609e185eedcd2c94202edfd09c4f8787fbb1f
Nurdaulet Satbergen <nurik93sat@gmail.com>
2026-08-01 00:36:37 +0500

    feat(swagger): annotate auth controller, add SignInDto for docs

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
@@ -6,13 +7,16 @@ import { GetUser } from './decorators/get-user.decorator';
 import { User } from '../../generated/prisma/client';
 import { Public } from './decorators/public.decorator';
 import type { JwtPayload } from './interfaces/jwt-payload.interface';
+import { SignInDto } from './dto/sign-in.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
   @UseGuards(LocalAuthGuard)
+  @ApiBody({ type: SignInDto })
   @Post('sign-in')
   signIn(@GetUser() user: Omit<User, 'password'>) {
     return this.authService.signIn(user);
@@ -24,6 +28,7 @@ export class AuthController {
     return this.authService.signUp(createUserDto);
   }
 
+  @ApiBearerAuth()
   @Get('profile')
   getProfile(@GetUser() user: JwtPayload) {
     return this.authService.getProfile(user.id);
